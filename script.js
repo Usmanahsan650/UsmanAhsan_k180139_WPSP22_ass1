05010733a96b38370e50c279aed61a01649dd4b4
Usman Ahsan <usmanahsan650@gmail.com>
2022-02-27 20:28:44 +0500

    Init4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded",()=>{
     document.getElementById("brand").addEventListener("click",()=>{
-        window.location.pathname="/home.html"
+        window.location.pathname="/UsmanAhsan_k180139_WPSP22_ass1/home.html"
     })
 
 })
